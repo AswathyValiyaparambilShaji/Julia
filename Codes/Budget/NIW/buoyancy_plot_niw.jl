407f5376e67e0f0f73e68a7546f21010ee9e26c3
AswathyValiyaparambilShaji <avaliyaparambilshaji@gmail.com>
2026-04-30 16:35:48 -0500

    heaving

--- Codes/Budget/NIW/buoyancy_plot_niw.jl
+++ Codes/Budget/NIW/buoyancy_plot_niw.jl
@@ -51,15 +51,16 @@ for xn in cfg["xn_start"]:cfg["xn_end"]
         ys = (yn - 1) * ty + 1
         ye = ys + ty - 1
 
-        println("$suffix")
+        #println("$suffix")
         G_buoy_full[xs:xe, ys:ye] .= g_buoy[buf+1:nx-buf, buf+1:ny-buf]
-        println(G_buoy_full[xs:xs+10,ys])
+        #println(G_buoy_full[xs:xs+10,ys])
 
 
         println("Completed tile $suffix")
     end
 end
 
+        println(G_buoy_full[10:10+10,10])
 
 println("\nG_buoy range: $(minimum(filter(!isnan, G_buoy_full))) to $(maximum(filter(!isnan, G_buoy_full)))")
 
