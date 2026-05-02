f926112df6d96a270c70b1b131d37adc12364843
AswathyValiyaparambilShaji <avaliyaparambilshaji@gmail.com>
2026-05-02 02:13:54 -0500

    heaving

--- job_multi.sh
+++ job_multi.sh
@@ -14,14 +14,14 @@ JOB2=$(qsub -v JULIA_SCRIPT=$BASE/NIW/buoyancy_pd_niw.jl -W depend=afterok:$JOB1
 echo "Job 2 submitted: $JOB2"
 
 
-JOB3=$(qsub -v JULIA_SCRIPT=$BASE/NIW/Shear_pdh_niw.jl -W depend=afterok:$JOB2 $PBS)
+JOB3=$(qsub -v JULIA_SCRIPT=$BASE/NIW/Shear_pdv_niw.jl -W depend=afterok:$JOB2 $PBS)
 echo "Job 3 submitted: $JOB3"
 
-JOB4=$(qsub -v JULIA_SCRIPT=$BASE/NIW/Shear_pdv_niw.jl -W depend=afterok:$JOB3 $PBS)
+JOB4=$(qsub -v JULIA_SCRIPT=$BASE/NT/UVW_nt_filtered.jl -W depend=afterok:$JOB3 $PBS)
 echo "Job 4 submitted: $JOB4"
 
-JOB5=$(qsub -v JULIA_SCRIPT=$BASE/NT/UVW_nt_filtered.jl -W depend=afterok:$JOB4 $PBS)
-echo "Job 5 submitted: $JOB5"
+#JOB5=$(qsub -v JULIA_SCRIPT=$BASE/NT/UVW_nt_filtered.jl -W depend=afterok:$JOB4 $PBS)
+#echo "Job 5 submitted: $JOB5"
 
 # Add as many scripts as you need following the same pattern...
 
