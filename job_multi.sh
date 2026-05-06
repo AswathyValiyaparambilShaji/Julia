b6b99487b48a4b2bf57a7e75bf07976cfa3cd71e
AswathyValiyaparambilShaji <avaliyaparambilshaji@gmail.com>
2026-05-06 01:09:01 -0500

    heaving

--- job_multi.sh
+++ job_multi.sh
@@ -6,40 +6,43 @@ PBS=/home3/avaliyap/Documents/Julia_new/Julia/m_run_j.pbs
 
 
 # Submit all jobs chained one after another
-#JOB1=$(qsub -v JULIA_SCRIPT=$BASE/NT/Ke_b_nt.jl  $PBS)
-#echo "Job 1 submitted: $JOB1"
+JOB1=$(qsub -v JULIA_SCRIPT=$BASE/NT/Ke_b_nt.jl  $PBS)
+echo "Job 1 submitted: $JOB1"
 
 #JOB2=$(qsub -v JULIA_SCRIPT=$BASE/NT/FlxDiv_plot_nt.jl  -W depend=afterok:$JOB1 $PBS)
 #echo "Job 2 submitted: $JOB2"
 
-#JOB3=$(qsub -v JULIA_SCRIPT=$BASE/NT/APE_nt.jl  -W depend=afterok:$JOB2 $PBS)
-#echo "Job 3 submitted: $JOB3"
+JOB3=$(qsub -v JULIA_SCRIPT=$BASE/NT/APE_nt.jl  -W depend=afterok:$JOB1 $PBS)
+echo "Job 3 submitted: $JOB3"
 
-#JOB4=$(qsub -v JULIA_SCRIPT=$BASE/NT/APE_plot_nt.jl  -W depend=afterok:$JOB3 $PBS)
-#echo "Job 4 submitted: $JOB4"
+JOB4=$(qsub -v JULIA_SCRIPT=$BASE/NT/APE_plot_nt.jl  -W depend=afterok:$JOB3 $PBS)
+echo "Job 4 submitted: $JOB4"
 
-#JOB5=$(qsub -v JULIA_SCRIPT=$BASE/NT/ADV_PE_nt_plot.jl  -W depend=afterok:$JOB4 $PBS)
-#echo "Job 5 submitted: $JOB5"
+JOB5=$(qsub -v JULIA_SCRIPT=$BASE/NT/ADV_PE_nt.jl  -W depend=afterok:$JOB4 $PBS)
+echo "Job 5 submitted: $JOB5"
 
-#JOB6=$(qsub -v JULIA_SCRIPT=$BASE/NT/ADV_PE_nt_plot.jl -W depend=afterok:$JOB5 $PBS)
-#echo "Job 6 submitted: $JOB6"
+JOB6=$(qsub -v JULIA_SCRIPT=$BASE/NT/ADV_PE_nt_plot.jl -W depend=afterok:$JOB5 $PBS)
+echo "Job 6 submitted: $JOB6"
 
 #JOB7=$(qsub -v JULIA_SCRIPT=$BASE/NT/buoyancy_pd_nt.jl -W depend=afterok:$JOB6 $PBS)
 #echo "Job 7 submitted: $JOB7"
 
 
 # Submit all jobs chained one after another
-JOB1=$(qsub -v JULIA_SCRIPT=$BASE/NIW/UVW_bpfilter.jl  $PBS)
-echo "Job 1 submitted: $JOB1"
+#JOB1=$(qsub -v JULIA_SCRIPT=$BASE/NIW/UVW_bpfilter.jl  $PBS)
+#echo "Job 1 submitted: $JOB1"
 
-JOB8=$(qsub -v JULIA_SCRIPT=$BASE/SM/UVW_sm_filtered.jl -W depend=afterok:$JOB1 $PBS)
-echo "Job 8 submitted: $JOB8"
+#JOB8=$(qsub -v JULIA_SCRIPT=$BASE/SM/UVW_sm_filtered.jl -W depend=afterok:$JOB1 $PBS)
+#echo "Job 8 submitted: $JOB8"
 
 JOB9=$(qsub -v JULIA_SCRIPT=$BASE/NIW/Shear_pdh_niw.jl -W depend=afterok:$JOB8 $PBS)
 echo "Job 9 submitted: $JOB9"
 
 JOB10=$(qsub -v JULIA_SCRIPT=$BASE/NIW/Shear_pdv_niw.jl -W depend=afterok:$JOB9 $PBS)
 echo "Job 10 submitted: $JOB10"
+
+JOB11=$(qsub -v JULIA_SCRIPT=$BASE/NIW/Shear_plot_niw.jl -W depend=afterok:$JOB10 $PBS)
+echo "Job 11 submitted: $JOB11"
 
 #JOB5=$(qsub -v JULIA_SCRIPT=$BASE -W depend=afterok:$JOB4 $PBS)
 #echo "Job 5 submitted: $JOB5"
