1ebeff4275b57f9c20bde6206fa03e0870cb0085
AswathyValiyaparambilShaji <avaliyaparambilshaji@gmail.com>
2026-05-10 02:22:10 -0500

    beam_ext

--- job_multi.sh
+++ job_multi.sh
@@ -15,7 +15,7 @@ echo "Job 2 submitted: $JOB2"
 JOB3=$(qsub -v JULIA_SCRIPT=$BASE/SM/SPH_sm_bc.jl  -W depend=afterok:$JOB2 $PBS)
 echo "Job 3 submitted: $JOB3"
 
-JOB4=$(qsub -v JULIA_SCRIPT=$BASE/SM/Shear_pdh_plot_nt.jl  -W depend=afterok:$JOB3 $PBS)
+JOB4=$(qsub -v JULIA_SCRIPT=$BASE/SM/Shear_pdh_plot_sm.jl  -W depend=afterok:$JOB3 $PBS)
 echo "Job 4 submitted: $JOB4"
 
 JOB5=$(qsub -v JULIA_SCRIPT=$BASE/SM/ADV_KE_sm_bc.jl  -W depend=afterok:$JOB4 $PBS)
@@ -38,7 +38,7 @@ echo "Job 6 submitted: $JOB6"
 JOB9=$(qsub -v JULIA_SCRIPT=$BASE/SM/ADV_KE_sm_plot.jl -W depend=afterok:$JOB6 $PBS)
 echo "Job 9 submitted: $JOB9"
 
-JOB10=$(qsub -v JULIA_SCRIPT=$BASE/SM/ADV_PE_sm_plot.jl -W depend=afterok:$JOB9 $PBS)
+JOB10=$(qsub -v JULIA_SCRIPT=$BASE/SM/ADV_PE_Plot_sm.jl -W depend=afterok:$JOB9 $PBS)
 echo "Job 10 submitted: $JOB10"
 
 #JOB11=$(qsub -v JULIA_SCRIPT=$BASE/NIW/Shear_plot_niw.jl -W depend=afterok:$JOB10 $PBS)
