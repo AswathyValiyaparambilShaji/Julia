d3fbcd91122d919caa16b8760e66e5641710eb9f
AswathyValiyaparambilShaji <avaliyaparambilshaji@gmail.com>
2026-05-18 21:14:26 -0500

    ds

--- job_multi.sh
+++ job_multi.sh
@@ -7,8 +7,8 @@ PBS=/home3/avaliyap/Documents/Julia_new/Julia/m_run_j.pbs
 
 
 # Submit all jobs chained one after another
-JOB1=$(qsub -v JULIA_SCRIPT=$BASE/SM/Buoyancy_pd_sm.jl  $PBS)
-echo "Job 1 submitted: $JOB1"
+#JOB1=$(qsub -v JULIA_SCRIPT=$BASE/SM/Buoyancy_pd_sm.jl  $PBS)
+#echo "Job 1 submitted: $JOB1"
 
 #JOB2=$(qsub -v JULIA_SCRIPT=$BASE/SM/Shear_pdv_plot_sm.jl  -W depend=afterok:$JOB1 $PBS)
 #echo "Job 2 submitted: $JOB2"
@@ -22,7 +22,7 @@ echo "Job 1 submitted: $JOB1"
 #JOB5=$(qsub -v JULIA_SCRIPT=$BASE/SM/ADV_KE_sm_bc.jl  -W depend=afterok:$JOB4 $PBS)
 #echo "Job 5 submitted: $JOB5"
 
-JOB6=$(qsub -v JULIA_SCRIPT=$BASE/SM/shear_pdh_sm.jl -W depend=afterok:$JOB1 $PBS)
+JOB6=$(qsub -v JULIA_SCRIPT=$BASE/SM/Shear_pdh_sm.jl $PBS)
 echo "Job 6 submitted: $JOB6"
 
 #JOB7=$(qsub -v JULIA_SCRIPT=$BASE/NT/buoyancy_pd_nt.jl -W depend=afterok:$JOB6 $PBS)
