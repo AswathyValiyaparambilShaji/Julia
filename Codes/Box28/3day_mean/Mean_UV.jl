8f3d3fe732f36fdc530497e92c891ab64f9fe5b5
AswathyValiyaparambilShaji <avaliyaparambilshaji@gmail.com>
2026-08-26 18:20:40 -0500

    NIW

--- Codes/Box28/3day_mean/Mean_UV.jl
+++ Codes/Box28/3day_mean/Mean_UV.jl
@@ -39,8 +39,8 @@ mkpath(joinpath(base, "3day_mean", "U"))
 mkpath(joinpath(base,"3day_mean", "V"))
 mkpath(joinpath(base,"3day_mean", "Salt"))
 mkpath(joinpath(base,"3day_mean", "Theta"))
-for xn in cfg["xn_start"]:cfg["xn_end"]
-    for yn in cfg["yn_start"]:cfg["yn_end"]
+for xn in cfg["xn_start"]:cfg["xn_e28"]
+    for yn in cfg["yn_start"]:cfg["yn_e28"]
 
 
         suffix = @sprintf("%02dx%02d_%d", xn, yn, buf)
--- Codes/Box28/NT/UVW_nt_filter.jl
+++ Codes/Box28/NT/UVW_nt_filter.jl
@@ -32,8 +32,8 @@ nt = 558
 
 
 # --- Loop over all tiles ---
-for xn in cfg["xn_start"]:cfg["xn_end"]
-    for yn in cfg["yn_start"]:cfg["yn_end"]
+for xn in cfg["xn_start"]:cfg["xn_e28"]
+    for yn in cfg["yn_start"]:cfg["yn_e28"]
 
 
         suffix = @sprintf("%02dx%02d_%d", xn, yn, buf)
