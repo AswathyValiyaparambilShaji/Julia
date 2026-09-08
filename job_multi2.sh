71a2c56ee4b37d9a772291779d021ef46baa738f
AswathyValiyaparambilShaji <avaliyaparambilshaji@gmail.com>
2026-09-08 12:04:26 -0500

    NIW

--- job_multi2.sh
+++ job_multi2.sh
@@ -7,26 +7,26 @@ PBS=/home3/avaliyap/Documents/Julia_new/Julia/m2_run_j.pbs
 
 
 # Submit all jobs chained one after another
-JOB1=$(qsub -v JULIA_SCRIPT=$BASE/Box28/NT/Conv_Plot_nt.jl  $PBS)
-echo "Job 1 submitted: $JOB1"
+#JOB1=$(qsub -v JULIA_SCRIPT=$BASE/Box28/NT/Conv_Plot_nt.jl  $PBS)
+#echo "Job 1 submitted: $JOB1"
 
-JOB2=$(qsub -v JULIA_SCRIPT=$BASE/Box28/NT/Flx_Div_Plot_nt.jl  $PBS)
-echo "Job 2 submitted: $JOB2"
+#JOB2=$(qsub -v JULIA_SCRIPT=$BASE/Box28/NT/Flx_Div_Plot_nt.jl  $PBS)
+#echo "Job 2 submitted: $JOB2"
 
 #OB3=$(qsub -v JULIA_SCRIPT=$BASE/Box28/.jl -W depend=afterok:$JOB2 $PBS)
 #echo "Job 3 submitted: $JOB3"
 
-JOB4=$(qsub -v JULIA_SCRIPT=$BASE/Box28/NT/Ke_plot_nt.jl -W depend=afterok:$JOB2 $PBS)
-echo "Job 4 submitted: $JOB4"
+#JOB4=$(qsub -v JULIA_SCRIPT=$BASE/Box28/NT/Ke_plot_nt.jl -W depend=afterok:$JOB2 $PBS)
+#echo "Job 4 submitted: $JOB4"
 
-JOB5=$(qsub -v JULIA_SCRIPT=$BASE/Box28/NT/APE_plot_nt.jl  -W depend=afterok:$JOB4 $PBS)
-echo "Job 5 submitted: $JOB5"
+#JOB5=$(qsub -v JULIA_SCRIPT=$BASE/Box28/NT/APE_plot_nt.jl  -W depend=afterok:$JOB4 $PBS)
+#echo "Job 5 submitted: $JOB5"
 
-JOB6=$(qsub -v JULIA_SCRIPT=$BASE/Box28/NT/ADV_KE_Plot_nt.jl -W depend=afterok:$JOB5 $PBS)
-echo "Job 6 submitted: $JOB6"
+#JOB6=$(qsub -v JULIA_SCRIPT=$BASE/Box28/NT/ADV_KE_Plot_nt.jl -W depend=afterok:$JOB5 $PBS)
+#echo "Job 6 submitted: $JOB6"
 
-JOB7=$(qsub -v JULIA_SCRIPT=$BASE/Box28/NT/ADV_PE_Plot_nt.jl -W depend=afterok:$JOB6 $PBS)
-echo "Job 7 submitted: $JOB7"
+#JOB7=$(qsub -v JULIA_SCRIPT=$BASE/Box28/NT/ADV_PE_Plot_nt.jl -W depend=afterok:$JOB6 $PBS)
+#echo "Job 7 submitted: $JOB7"
 
 
 # Submit all jobs chained one after another
@@ -36,7 +36,7 @@ echo "Job 7 submitted: $JOB7"
 #JOB8=$(qsub -v JULIA_SCRIPT=$BASE/Box28/NT/Tendency_term_nt.jl -W depend=afterok:$JOB7 $PBS)
 #echo "Job 8 submitted: $JOB8"
 
-JOB9=$(qsub -v JULIA_SCRIPT=$BASE/Box28/NT/Shear_pdh_Plot_nt.jl -W depend=afterok:$JOB7 $PBS)
+JOB9=$(qsub -v JULIA_SCRIPT=$BASE/Box28/NT/Shear_pdv_nt.jl  $PBS)
 echo "Job 9 submitted: $JOB9"
 
 JOB10=$(qsub -v JULIA_SCRIPT=$BASE/Box28/NT/Shear_pdv_Plot_nt.jl -W depend=afterok:$JOB9 $PBS)
