07e87d2900ec32fd1df36ab0a9ecb0436e035ae0
AswathyValiyaparambilShaji <avaliyaparambilshaji@gmail.com>
2026-09-18 13:27:03 -0500

    NIW

--- job_multi2.sh
+++ job_multi2.sh
@@ -36,7 +36,7 @@ PBS=/home3/avaliyap/Documents/Julia_new/Julia/m2_run_j.pbs
 JOB8=$(qsub -v JULIA_SCRIPT=$BASE/Box27b/NT/APE_plot_nt.jl  $PBS)
 echo "Job 8 submitted: $JOB8"
 
-JOB9=$(qsub -v JULIA_SCRIPT=$BASE/Box27b/NT/Energy_Budget_nt.jl -W depend=afterok:$JOB8 $PBS)
+JOB9=$(qsub -v JULIA_SCRIPT=$BASE/Box27b/NT/Energy_budget_nt.jl -W depend=afterok:$JOB8 $PBS)
 echo "Job 9 submitted: $JOB9"
 
 #JOB10=$(qsub -v JULIA_SCRIPT=$BASE/Box28/NT/Energy_Budget_wkly_nt.jl -W depend=afterok:$JOB9 $PBS)
